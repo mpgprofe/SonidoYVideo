af71ac3b8b25d69bfa330d2b27ba7affd3d6d87b
mpgprofe <mpgprofe@gmail.com>
2020-10-29 14:13:35 +0100

    le ponemos una barra para mover la música y ver por dónde va.

--- app/src/main/java/com/example/sonidoyvideo/MainActivity.java
+++ app/src/main/java/com/example/sonidoyvideo/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -19,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
     VideoView videoView;
     MediaPlayer mediaPlayer;
     MediaController mediaController;
-
+    SeekBar seekBar;
+BarraMusica barraMusica;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         stop = findViewById(R.id.buttonStop);
         textView = findViewById(R.id.textView);
         videoView = findViewById(R.id.videoView);
+        seekBar = findViewById(R.id.seekBar);
 
         mediaPlayer = MediaPlayer.create(this, R.raw.dualipa);
 
@@ -38,6 +42,10 @@ public class MainActivity extends AppCompatActivity {
                 if (mediaPlayer.isPlaying()) {
                     textView.setText("Ya está sonando!!");
                 } else {
+                    if (barraMusica==null){
+                        barraMusica = new BarraMusica(seekBar,mediaPlayer);
+                        barraMusica.execute();
+                    }
                     mediaPlayer.start();
                     textView.setText("La canción está sonando");
                 }
@@ -73,6 +81,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+            }
+        });
 
         mediaController = new MediaController(this);
         mediaController.setAnchorView(videoView);
@@ -91,5 +115,38 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+    class BarraMusica extends AsyncTask<String, String, String>{
+        SeekBar miSeekBar;
+        MediaPlayer miMediaPlayer;
+        BarraMusica(SeekBar sb, MediaPlayer mp){
+            miSeekBar=sb;
+            miMediaPlayer = mp;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            miSeekBar.setMax(miMediaPlayer.getDuration());
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            miSeekBar.setProgress(miMediaPlayer.getCurrentPosition());
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            while (true){
+                if (mediaPlayer.isPlaying()){
+                    publishProgress();
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }
     }
 }
